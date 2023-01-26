1db3caf8742f7447406f31ab819fd0b8a5b0deb0
valentinazhbanova <vala.ko@mail.ru>
2023-01-26 10:19:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+    const [trip, setTrip] = useState(0);
+    const {id, name, image, description} = data[trip];
+
+const perviousTrip = () => {
+    setTrip((trip => {
+    trip --;
+    if(trip < 0) {
+        return data.length -1;
+        }
+        return trip;
+    }))
+}
+
+    const nextTrip = () => {
+    setTrip((trip => {
+        trip ++;
+        if (trip > data.length -1){
+        trip = 0;
+        }
+        return trip;
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <h2>Города Дании</h2>
+      </div>
+      <div className='container'>
+        <img src={image} width='550px' alt='Города Дании'/>
+      </div>
+      <div className='container btn'> 
+        <button onClick={perviousTrip}>Previous</button>
+        <button onClick={nextTrip}>Next</button>
+      </div>
+      <div className='container'>
+        <h2>{name}</h2>
+      </div>
+      <div className='container'>
+        <h3 className='description'>{description}</h3>
+      </div>
     </div>
   );
 }
